2603efaee0770b516d16db3038d471e806936443
Filip Aleksovski <filip.aleksovski77@gmail.com>
2018-03-02 20:37:43 +0100

    NavBar

--- app/src/main/java/com/example/korisnik/githubapi/MainActivity.java
+++ app/src/main/java/com/example/korisnik/githubapi/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.korisnik.githubapi;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private Button search_button;
 
     private EditText search_edit_text;
+    private BottomNavigationView bottomNavigationView;
 
     private String BASE_URL = "https://api.github.com/";
 
@@ -42,7 +46,18 @@ public class MainActivity extends AppCompatActivity {
 
         search_button = findViewById(R.id.search_button);
         search_edit_text =findViewById(R.id.search_edit_text);
+        bottomNavigationView=findViewById(R.id.bottom_nav);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.like_menu:
+
+                }
+                return true;
+            }
+        });
 
         search_button.setOnClickListener(new View.OnClickListener() {
             @Override
